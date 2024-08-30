0ca9c27595bf0a05fbda4afac3749e1d0fbebfa4
koleso8 <dev.koleso@gmail.com>
2024-08-30 08:43:28 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
 import './style.css';
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// const name: string = 'Bob';
+// const lastName: string = 'Pes';
+// const age: number = 19;
+// const status: boolean = true;
+// const budjet: null = null;
+// const statusQ: undefined = undefined;
 
-console.log(add(1, 2));
+//todo Create a TypeScript function named displayUserProfile that takes a user profile object as an argument. The user profile object should have the following properties: username (string), age (number), and isActive (boolean). The function should return a string summarizing the user's profile details.
+
+// type userProfile = {
+//   userName: string;
+//   age: number;
+//   isActive: boolean;
+// };
+
+// const user = {
+//   userName: 'Bob',
+//   age: 12,
+//   isActive: true,
+// };
+
+// function displayUserProfile(user: userProfile) {
+//   return `User name is ${user.userName} his is ${user.age} years old and his is ${user.isActive}`;
+// }
+
+// console.log(displayUserProfile(user));
+
+// function logName(name: string) {
+//   return name;
+// }
+// console.log(logName('pes'));
+
+// function calcTotal(arr: { asd: number }[]) {
+//   return arr.reduce((acc, el) => acc + el.asd, 10);
+// }
+// const numbers = [{ asd: 10 }, { asd: 20 }, { asd: 30 }, { asd: 40 }];
+// console.log(calcTotal(numbers));
